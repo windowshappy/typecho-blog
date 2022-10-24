6244256fa9a1efe9ce0ed8cbe35c6381db2e02a0
windowshappy <13911821940@163.com>
2022-10-24 11:23:57 +0800

    fix icon

--- usr/themes/Single/header.php
+++ usr/themes/Single/header.php
@@ -16,7 +16,8 @@
 <?php endif; ?>
     <link href="<?php $this -> options -> themeUrl('static/kico.css'); ?>" rel="stylesheet" type="text/css"/>
     <link href="<?php $this -> options -> themeUrl('static/single.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <!-- <link href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/> -->
+    <script src="https://use.fontawesome.com/bcf2c78fce.js"></script>
     <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1"/>
 <?php if($this -> options -> background): ?>
     <style>body:before{content: ''; background-image: url(<?php $this -> options -> background(); ?>)}</style>
